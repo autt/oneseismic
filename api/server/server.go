5e09daf3adb36cf309680c78dd3e04f77cd812bf
Kjell W. Kongsvik <kjell.kongsvik@gmail.com>
2020-07-01 15:18:01 +0200

    Send storageEndpoint to core

--- api/server/server.go
+++ api/server/server.go
@@ -1,12 +1,14 @@
 package server
 
 import (
+	"net/url"
+
 	"github.com/google/uuid"
 	"github.com/kataras/iris/v12"
 )
 
-func registerStoreController(app *iris.Application, storageURL, accountName, accountKey string) error {
-	sURL, err := newServiceURL(storageURL, accountName, accountKey)
+func registerStoreController(app *iris.Application, storageEndpoint url.URL, accountName, accountKey string) error {
+	sURL, err := newServiceURL(storageEndpoint, accountName, accountKey)
 	if err != nil {
 		return err
 	}
@@ -20,31 +22,32 @@ func registerStoreController(app *iris.Application, storageURL, accountName, acc
 	return nil
 }
 
-func RegisterSlicer(
+func registerSlicer(
 	app *iris.Application,
+	storageEndpoint string,
 	reqNdpt string,
 	repNdpt string,
 	root string,
 	mPlexName string,
 ) {
-	sc := createSliceController(reqNdpt, repNdpt, root, mPlexName)
+	sc := createSliceController(reqNdpt, repNdpt, storageEndpoint, root, mPlexName)
 
 	app.Get("/{guid:string}/slice/{dim:int32}/{lineno:int32}", sc.get)
 }
 
 func Register(
 	app *iris.Application,
-	storageURL,
+	storageEndpoint url.URL,
 	accountName,
 	accountKey,
 	reqNdpt,
 	repNdpt string,
 ) error {
-	if err := registerStoreController(app, storageURL, accountName, accountKey); err != nil {
+	if err := registerStoreController(app, storageEndpoint, accountName, accountKey); err != nil {
 		return err
 	}
 	mPlexName := uuid.New().String()
-	RegisterSlicer(app, reqNdpt, repNdpt, accountName, mPlexName)
+	registerSlicer(app, storageEndpoint.String(), reqNdpt, repNdpt, accountName, mPlexName)
 
 	return nil
 }
